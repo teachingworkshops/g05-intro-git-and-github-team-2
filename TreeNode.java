1b96d262aca74aa7e44aa9ed6b863778021d3361
Alexander Men <mena1@wit.edu>
2024-01-25 11:50:47 -0500

    changed combat state to pass in as an arg instead

--- TreeNode.java
+++ TreeNode.java
@@ -4,16 +4,15 @@ import java.util.List;
 public class TreeNode {
     private List<TreeNode> adjacent;
     private String text;
-    private Player player;
 
     public TreeNode(String text) {
         this.text = text;
         this.adjacent = new ArrayList<>();
     }
 
-    public void addAdjacent(TreeNode node) {
+    public void addAdjacent(TreeNode node, boolean combat) {
         adjacent.add(node);
-        if (player.combatState() == false){
+        if (combat == false){
             node.adjacent.add(this);
         }
     }
